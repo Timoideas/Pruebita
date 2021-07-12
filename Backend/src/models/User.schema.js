b45be7558b02c1edd6274ab5b9ba4f7309797f08
Timoideas <Timoideas@hotmail.com>
2021-07-12 17:46:51 -0500

    MOdel

--- Backend/src/models/User.schema.js
+++ Backend/src/models/User.schema.js
@@ -1,7 +1,11 @@
 import { Schema, model } from 'mongoose';
 import validator from 'mongoose-unique-validator';
 const UserSchema = new Schema({
-  name: { type: String },
+  Name: { type: String },
+  Userame: { type: String },
+  Password: { type: String },
+  Email: { type: String },
+  Telefono: { type: String },
 });
 UserSchema.plugin(validator, { message: 'El {PATH} debería ser único' });
 export default model('User', UserSchema);
