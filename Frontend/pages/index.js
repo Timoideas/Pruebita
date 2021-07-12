561942e637996c8443acbab111d8f24d9872d402
Timoideas <Timoideas@hotmail.com>
2021-07-12 17:05:15 -0500

    asd

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,11 +1,10 @@
-import style from "styles/pages/Index.module.css";
-import Head_Main from "heads/Main.head";
+import style from 'styles/pages/Index.module.css';
+import Head_Main from 'heads/Main.head';
 import {
   Body,
   Section,
   Content,
-} from "components/timoideas/Timoideas.components";
-import BienvenidaSockets from "components/SocketsSaludo.component";
+} from 'components/timoideas/Timoideas.components';
 
 export default function Index() {
   return (
@@ -14,8 +13,11 @@ export default function Index() {
       <Body>
         <Section>
           <Content center flex={0.5}>
-            <h1 className={style.Title}>Welcome</h1>
-            <BienvenidaSockets />
+            <input type='text' placeholder='name' />
+            <input type='text' placeholder='username' />
+            <input type='text' placeholder='password' />
+            <input type='text' placeholder='email' />
+            <input type='text' placeholder='telefono' />
           </Content>
         </Section>
       </Body>
